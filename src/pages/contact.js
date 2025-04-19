3c62fad08d6667ac5d8832706cda6c857bc41fb9
Maykal Droshev <72551672+MaykalDroshev-Hull@users.noreply.github.com>
2025-04-19 16:59:56 +0100

    finished initial design of the index page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -85,7 +85,7 @@ const contact = () => {
     <>
       <Meta
         title="Контакти"
-        description="Get in touch with Asea-M Auto Detailing and take the first step towards a cleaner, more polished car. Our experienced team is here to answer your questions and help you schedule an appointment for interior and exterior detailing, paint correction, ceramic coating, and more. Contact us today and experience the highest level of customer service and quality workmanship."
+        description="Get in touch with .-M Auto Detailing and take the first step towards a cleaner, more polished car. Our experienced team is here to answer your questions and help you schedule an appointment for interior and exterior detailing, paint correction, ceramic coating, and more. Contact us today and experience the highest level of customer service and quality workmanship."
         keywords="auto detailing, car detailing, detailing services, paint correction, ceramic coating, interior detailing, exterior detailing, car care, auto appearance, auto restoration, contact us, schedule an appointment, get in touch, phone number, email address, location"
       />
       <PageTitle title="Контакти" />
