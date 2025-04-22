033f731627bd6b9b0f7f6f2b30ad991b19d20660
Maykal Droshev <72551672+MaykalDroshev-Hull@users.noreply.github.com>
2025-04-22 23:14:06 +0100

    Added analytics

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,6 +2,7 @@ import NavBar from './Layout Components/NavBar'
 //import Transition from './Layout Components/Transition'
 import Footer from './Layout Components/Footer'
 import styles from '../styles/Component Styles/Layout.module.css'
+import { Analytics } from "@vercel/analytics/react"
 
 /**
  * A layout component that defines the overall structure of the web page
@@ -13,6 +14,7 @@ const Layout = ({ children }) => {
   return (
     <>
       <div className={styles.layout}>
+      <Analytics/>
         <NavBar />
         {/* <Transition> */}
         <main className={styles.main}>
